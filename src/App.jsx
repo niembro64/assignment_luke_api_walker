c2088a3fdc3fc2345e877f762888b70f0b0327b1
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-19 19:47:59 -0400

    arbitrary calls

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,26 @@ function App() {
     { name: "name1", age: "age1", haircolor: "brown" },
   ]);
   const [num, setNum] = useState(1);
+  const [ent, setEnt] = useState("people");
 
   const p = (a) => {
     console.log(a);
   };
 
   useEffect(() => {
+    // p("useEffect Running");
     axios.get("https://swapi.dev/api/").then((response) => {
       const keys = Object.keys(response.data);
       setList(keys);
     });
-  }, [list]);
+  }, []);
 
   const onClickHandler = (event) => {
     event.preventDefault();
 
-    console.log("calling axios people");
-    axios.get("https://swapi.dev/api/people/1/").then((response) => {
+    console.log(`calling axios ent: ${ent} num: ${num} `);
+    // axios.get("https://swapi.dev/api/people/1/").then((response) => {
+    axios.get("https://swapi.dev/api/" + ent + "/" + num + "/").then((response) => {
       console.log(response.data);
       setEntity(response.data);
     });
@@ -41,20 +44,33 @@ function App() {
     <div className="App">
       <h1>Luke API Walker</h1>
       <form>
-        <select name="x_type" className="options">
+        <select
+          name="x_type"
+          defaultValue="people"
+          className="options"
+          onChange={(event) => {
+            setEnt(event.target.value);
+          }}
+        >
           {list.map((item, i) => {
             return (
-              <option
-                className="options"
-                key={i}
-                value={item}
-              >
+              <option className="options" key={i} value={item}>
                 {item}
               </option>
             );
           })}
         </select>
-        <input name="x_num" className="options" type="number" min="1" max="10" defaultValue="1" onChange={(event)=>{setNum(event.target.value)}}/>
+        <input
+          name="x_num"
+          className="options"
+          type="number"
+          min="1"
+          max="10"
+          defaultValue="1"
+          onChange={(event) => {
+            setNum(event.target.value);
+          }}
+        />
         <button className="btn btn-primary mx-4" onClick={onClickHandler}>
           Go
         </button>
@@ -76,6 +92,9 @@ function App() {
       </div>
       <div className="box">
         <p>{num}</p>
+      </div>
+      <div className="box">
+        <p>{ent}</p>
       </div>
     </div>
   );
