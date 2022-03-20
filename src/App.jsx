8df32345df01abc7c1fdbf2916f654d79a232082
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-20 01:27:36 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, createElement } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
-  const [list, setList] = useState(["list1", "list2", "list3"]);
-  const [listInfo, setListInfo] = useState([
-    ["person", "name", "haircolor"],
-    ["planet", "size", "color"],
-    ["gun", "power", "color"],
+  const [list, setList] = useState([
+    "list1",
+    "list2",
+    "list3",
+    "list4",
+    "list5",
   ]);
   const [entity, setEntity] = useState([
-    { name: "name1", age: "age1", haircolor: "brown" },
+    {
+      name: "name1",
+      age: "age1",
+      haircolor: "brown",
+      four: "four",
+      five: "five",
+      six: "six",
+    },
   ]);
   const [num, setNum] = useState(1);
   const [ent, setEnt] = useState("people");
+  const [help, setHelp] = useState(true);
 
   const p = (a) => {
     console.log(a);
@@ -27,17 +36,20 @@ function App() {
       const keys = Object.keys(response.data);
       setList(keys);
     });
-  }, []);
+  }, [entity, list, num, ent, help]);
 
   const onClickHandler = (event) => {
     event.preventDefault();
 
     console.log(`calling axios ent: ${ent} num: ${num} `);
     // axios.get("https://swapi.dev/api/people/1/").then((response) => {
-    axios.get("https://swapi.dev/api/" + ent + "/" + num + "/").then((response) => {
-      console.log(response.data);
-      setEntity(response.data);
-    });
+    axios
+      .get("https://swapi.dev/api/" + ent + "/" + num + "/")
+      .then((response) => {
+        console.log(response.data);
+        setEntity(response.data);
+        setHelp(false);
+      });
   };
 
   return (
@@ -65,7 +77,7 @@ function App() {
           className="options"
           type="number"
           min="1"
-          max="10"
+          max="100"
           defaultValue="1"
           onChange={(event) => {
             setNum(event.target.value);
@@ -75,27 +87,33 @@ function App() {
           Go
         </button>
       </form>
-      <div className="box">
-        {list.map((item, i) => {
-          return <p key={i}>{item}</p>;
-        })}
-      </div>
-      <div className="box">
-        {entity.map((item, i) => {
-          return (
-            <>
-              {<p>{item.name}</p>}
-              {<p>{item.haircolor}</p>}
-            </>
-          );
-        })}
-      </div>
-      <div className="box">
-        <p>{num}</p>
-      </div>
-      <div className="box">
-        <p>{ent}</p>
-      </div>
+      {/* <div className="box">
+        <p>Ent: {ent}</p>
+        <p>Num: {num}</p>
+      </div> */}
+      {help ? (
+        ""
+      ) : (
+        <div className="box">
+          <h1>{Object.values(entity)[0]}</h1>
+          <div className="pboy">
+            <p>{Object.keys(entity)[1]}</p>
+            <p>{Object.values(entity)[1]}</p>
+          </div>
+          <div className="pboy">
+            <p> {Object.keys(entity)[2]}</p>
+            <p> {Object.values(entity)[2]}</p>
+          </div>
+          <div className="pboy">
+            <p> {Object.keys(entity)[3]}</p>
+            <p>{Object.values(entity)[3]}</p>
+          </div>
+          <div className="pboy">
+            <p>{Object.keys(entity)[4]}</p>
+            <p>{Object.values(entity)[4]}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
